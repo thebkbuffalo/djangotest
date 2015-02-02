ba162574e7e2637a6e50a00b79a5e2a5195f7a3d
thebkbuffalo <evan.m.berg@gmail.com>
2015-02-02 17:17:48 -0500

    updated admin panel with filters and search, on customizing the look of admint panel now

--- polls/admin.py
+++ polls/admin.py
@@ -12,7 +12,10 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
+
 
 # Register your models here.
